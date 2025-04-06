aa1d0a4adc93755977cab669e77411c2814a7bb2
adnan-hajro <adnan.hajro@ibu.edu.ba>
2025-04-06 14:32:38 +0200

    Manifest changes + build.gradle.kts

--- app/src/main/java/ibu/edu/ba/myapplication/MainActivity.kt
+++ app/src/main/java/ibu/edu/ba/myapplication/MainActivity.kt
@@ -1,29 +1,55 @@
 package ibu.edu.ba.myapplication
 
+import ibu.edu.ba.myapplication.ui.screens.CustomGalleryView
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ibu.edu.ba.myapplication.ui.ImageViewModel
+import ibu.edu.ba.myapplication.ui.screens.SendEmailView
 import ibu.edu.ba.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<ImageViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                SendEmailView()
+                //CustomGalleryView(viewModel = viewModel)
+            }
+        }
+
+//        intent?.let {
+//            if (it.action == Intent.ACTION_SEND && it.type?.startsWith("image/") == true) {
+//                val imageUri: Uri? = it.getParcelableExtra(Intent.EXTRA_STREAM)
+//                imageUri?.let { uri ->
+//                    viewModel.addImage(uri) // Update ViewModel with the shared image
+//                }
+//            }
+//        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d("Main activity: ", "onNewIntent called")
+        intent.let {
+            if (it.action == Intent.ACTION_SEND && it.type?.startsWith("image/") == true) {
+                val imageUri: Uri? = it.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+                imageUri?.let { uri ->
+                    viewModel.addImage(uri) // Update ViewModel with the shared image
                 }
             }
         }
